7dc30ce1e94c96a59e5a01920535f9f4fcb22d03
Blackjubel <blackjubels@gmail.com>
2016-08-22 21:31:36 +0200

    Update product_debug.py

--- tools/product_debug.py
+++ tools/product_debug.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright (C) 2009 The Android Open Source Project
+# Copyright (C) 2012 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,143 +14,146 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+import re
 import sys
 
+def break_lines(key, val):
+  # these don't get split
+  if key in ("PRODUCT_MODEL"):
+    return (key,val)
+  return (key, "\n".join(val.split()))
 
-def iteritems(obj):
-  if hasattr(obj, 'iteritems'):
-    return obj.iteritems()
-  return obj.items()
+def split_line(line):
+  words = line.split("=", 1)
+  if len(words) == 1:
+    return (words[0], "")
+  else:
+    return (words[0], words[1])
 
+def sort_lines(text):
+  lines = text.split()
+  lines.sort()
+  return "\n".join(lines)
 
-# Usage: post_process_props.py file.prop [blacklist_key, ...]
-# Blacklisted keys are removed from the property file, if present
+def parse_variables(lines):
+  return [split_line(line) for line in lines if line.strip()]
 
-# See PROP_NAME_MAX and PROP_VALUE_MAX system_properties.h.
-# The constants in system_properties.h includes the termination NUL,
-# so we decrease the values by 1 here.
-PROP_NAME_MAX = 31
-PROP_VALUE_MAX = 91
+def render_variables(variables):
+  variables = dict(variables)
+  del variables["FILE"]
+  variables = list(variables.iteritems())
+  variables.sort(lambda a, b: cmp(a[0], b[0]))
+  return ("<table id='variables'>"
+      + "\n".join([ "<tr><th>%(key)s</th><td>%(val)s</td></tr>" % { "key": key, "val": val }
+        for key,val in variables])
+      +"</table>")
 
-# Put the modifications that you need to make into the /system/build.prop into this
-# function. The prop object has get(name) and put(name,value) methods.
-def mangle_build_prop(prop, overrides):
-  if len(overrides) == 0:
-    return
-  overridelist = overrides.replace(" ",",").split(",")
-  for proppair in overridelist:
-    values = proppair.split("=")
-    prop.put(values[0], values[1])
+def linkify_inherit(variables, text, func_name):
+  groups = re.split("(\\$\\(call " + func_name + ",.*\\))", text)
+  result = ""
+  for i in range(0,len(groups)/2):
+    i = i * 2
+    result = result + groups[i]
+    s = groups[i+1]
+    href = s.split(",", 1)[1].strip()[:-1]
+    href = href.replace("$(SRC_TARGET_DIR)", "build/target")
+    href = ("../" * variables["FILE"].count("/")) + href + ".html"
+    result = result + "<a href=\"%s\">%s</a>" % (href,s)
+  result = result + groups[-1]
+  return result
 
-  pass
+def render_original(variables, text):
+  text = linkify_inherit(variables, text, "inherit-product")
+  text = linkify_inherit(variables, text, "inherit-product-if-exists")
+  return text
 
-# Put the modifications that you need to make into the /default.prop into this
-# function. The prop object has get(name) and put(name,value) methods.
-def mangle_default_prop(prop):
-  # If ro.build.type is userdebug, then enable adb on USB by default
-  if prop.get("ro.build.type") == "userdebug":
-    val = prop.get("persist.sys.usb.config")
-    if val == "":
-      val = "adb"
-    else:
-      val = val + ",adb"
-    prop.put("persist.sys.usb.config", val)
-  # UsbDeviceManager expects a value here.  If it doesn't get it, it will
-  # default to "adb". That might not the right policy there, but it's better
-  # to be explicit.
-  if not prop.get("persist.sys.usb.config"):
-    prop.put("persist.sys.usb.config", "none")
-
-def validate(prop):
-  """Validate the properties.
-
-  Returns:
-    True if nothing is wrong.
-  """
-  check_pass = True
-  buildprops = prop.to_dict()
-  for key, value in iteritems(buildprops):
-    # Check build properties' length.
-    if len(key) > PROP_NAME_MAX:
-      check_pass = False
-      sys.stderr.write("error: %s cannot exceed %d bytes: " %
-                       (key, PROP_NAME_MAX))
-      sys.stderr.write("%s (%d)\n" % (key, len(key)))
-    if len(value) > PROP_VALUE_MAX:
-      check_pass = False
-      sys.stderr.write("error: %s cannot exceed %d bytes: " %
-                       (key, PROP_VALUE_MAX))
-      sys.stderr.write("%s (%d)\n" % (value, len(value)))
-  return check_pass
-
-class PropFile:
-
-  def __init__(self, lines):
-    self.lines = [s.strip() for s in lines]
-
-  def to_dict(self):
-    props = {}
-    for line in self.lines:
-      if not line or line.startswith("#"):
-        continue
-      if "=" in line:
-        key, value = line.split("=", 1)
-        props[key] = value
-    return props
-
-  def get(self, name):
-    key = name + "="
-    for line in self.lines:
-      if line.startswith(key):
-        return line[len(key):]
-    return ""
-
-  def put(self, name, value):
-    key = name + "="
-    for i in range(0,len(self.lines)):
-      if self.lines[i].startswith(key):
-        self.lines[i] = key + value
-        return
-    self.lines.append(key + value)
-
-  def delete(self, name):
-    key = name + "="
-    self.lines = [ line for line in self.lines if not line.startswith(key) ]
-
-  def write(self, f):
-    f.write("\n".join(self.lines))
-    f.write("\n")
+def read_file(fn):
+  f = file(fn)
+  text = f.read()
+  f.close()
+  return text
 
 def main(argv):
-  filename = argv[1]
-  if (len(argv) > 2):
-    extraargs = argv[2]
-  else:
-    extraargs = ""
-  f = open(filename)
-  lines = f.readlines()
-  f.close()
+  # read the variables
+  lines = sys.stdin.readlines()
+  variables = parse_variables(lines)
 
-  properties = PropFile(lines)
+  # format the variables
+  variables = [break_lines(key,val) for key,val in variables]
 
-  if filename.endswith("/build.prop"):
-    mangle_build_prop(properties, extraargs)
-  elif filename.endswith("/default.prop"):
-    mangle_default_prop(properties)
-  else:
-    sys.stderr.write("bad command line: " + str(argv) + "\n")
-    sys.exit(1)
+  # now it's a dict
+  variables = dict(variables)
 
-  if not validate(properties):
-    sys.exit(1)
+  sorted_vars = (
+      "PRODUCT_COPY_FILES",
+      "PRODUCT_PACKAGES",
+      "PRODUCT_LOCALES",
+      "PRODUCT_PROPERTY_OVERRIDES",
+    )
 
-  # Drop any blacklisted keys
-  for key in argv[3:]:
-    properties.delete(key)
+  for key in sorted_vars:
+    variables[key] = sort_lines(variables[key])
 
-  f = open(filename, 'w+')
-  properties.write(f)
-  f.close()
+  # the original file
+  original = read_file(variables["FILE"])
+
+  # formatting
+  values = dict(variables)
+  values.update({
+    "variables": render_variables(variables),
+    "original": render_original(variables, original),
+  })
+  print """<html>
+
+
+<head>
+  <title>%(FILE)s</title>
+  <style type="text/css">
+    body {
+      font-family: Helvetica, Arial, sans-serif;
+      padding-bottom: 20px;
+    }
+    #variables {
+      border-collapse: collapse;
+    }
+    #variables th, #variables td {
+      vertical-align: top;
+      text-align: left;
+      border-top: 1px solid #c5cdde;
+      border-bottom: 1px solid #c5cdde;
+      padding: 2px 10px 2px 10px;
+    }
+    #variables th {
+      font-size: 10pt;
+      background-color: #e2ecff
+    }
+    #variables td {
+      background-color: #ebf2ff;
+      white-space: pre;
+      font-size: 10pt;
+    }
+    #original {
+      background-color: #ebf2ff;
+      border-top: 1px solid #c5cdde;
+      border-bottom: 1px solid #c5cdde;
+      padding: 2px 10px 2px 10px;
+      white-space: pre;
+      font-size: 10pt;
+    }
+  </style>
+</head>
+<body>
+<h1>%(FILE)s</h1>
+<a href="#Original">Original</a>
+<a href="#Variables">Variables</a>
+<h2><a name="Original"></a>Original</h2>
+<div id="original">%(original)s</div>
+<h2><a name="Variables"></a>Variables</h2>
+%(variables)s
+</body>
+</html>
+""" % values
 
 if __name__ == "__main__":
   main(sys.argv)
